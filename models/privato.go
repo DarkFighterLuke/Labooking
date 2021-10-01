f2affdac6d4af560bb4f17f7e7b0735ddd1f855a
DarkFighterLuke <luca-consoli@live.it>
2021-10-01 12:15:22 +0200

    Aggiunti tag ORM a privato.go

--- models/privato.go
+++ models/privato.go
@@ -3,20 +3,20 @@ package models
 import "time"
 
 type Privato struct {
-	IdPrivato              int
-	Nome                   string
-	Cognome                string
-	CodiceFiscale          string
-	NumeroTesseraSanitaria string
-	Citta                  string
-	Cap                    string
-	Via                    string
-	Civico                 int
-	Prefisso               string
-	Telefono               string
-	Email                  string
+	IdPrivato              int    `orm:"pk;auto"`
+	Nome                   string `orm:"size(255)"`
+	Cognome                string `orm:"size(255)"`
+	CodiceFiscale          string `orm:"size(16);unique"`
+	NumeroTesseraSanitaria string `orm:"size(20);unique"`
+	Citta                  string `orm:"size(255)"`
+	Cap                    string `orm:"size(5)"`
+	Via                    string `orm:"size(255)"`
+	Civico                 int    `orm:"digits(4)"`
+	Prefisso               string `orm:"size(2)"`
+	Telefono               string `orm:"size(10);unique"`
+	Email                  string `orm:"size(255);unique"`
 	EmailConfermata        bool
-	Psw                    string
-	DataNascita            time.Time
-	// TODO: Aggiungere puntatore a medico
+	Psw                    string    `orm:"size(255)"`
+	DataNascita            time.Time `orm:"type(date)"`
+	Medico                 *Medico   `orm:"rel(fk)"`
 }
