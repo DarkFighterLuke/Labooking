105ff26de15de107ed48a353cb75b699d8b41d73
DarkFighterLuke <luca-consoli@live.it>
2021-10-04 16:11:49 +0200

    Spostata funzione underAgeValidate() da registrazione_privato.js a registrazione.js

--- static/js/registrazione/registrazione.js
+++ static/js/registrazione/registrazione.js
@@ -1,2 +1,19 @@
 initElementsPrivato();
 initElementsMedico();
+
+function underAgeValidate(birthday){
+    // it will accept two types of format yyyy-mm-dd and yyyy/mm/dd
+    let optimizedBirthday = birthday.replace(/-/g, "/");
+
+    //set date based on birthday at 01:00:00 hours GMT+0100 (CET)
+    let myBirthday = new Date(optimizedBirthday);
+
+    // set current day on 01:00:00 hours GMT+0100 (CET)
+    let currentDate = new Date().toJSON().slice(0,10)+' 01:00:00';
+
+    // calculate age comparing current date and borthday
+    let myAge = ~~((Date.now(currentDate) - myBirthday) / (31557600000));
+
+    return myAge >= 18;
+
+}
--- static/js/registrazione/registrazione_privato.js
+++ static/js/registrazione/registrazione_privato.js
@@ -178,23 +178,6 @@ function checkDataNascitaPrivato(){
     }
 }
 
-function underAgeValidate(birthday){
-    // it will accept two types of format yyyy-mm-dd and yyyy/mm/dd
-    let optimizedBirthday = birthday.replace(/-/g, "/");
-
-    //set date based on birthday at 01:00:00 hours GMT+0100 (CET)
-    let myBirthday = new Date(optimizedBirthday);
-
-    // set current day on 01:00:00 hours GMT+0100 (CET)
-    let currentDate = new Date().toJSON().slice(0,10)+' 01:00:00';
-
-    // calculate age comparing current date and borthday
-    let myAge = ~~((Date.now(currentDate) - myBirthday) / (31557600000));
-
-    return myAge >= 18;
-
-}
-
 function submitPrivato(){
     if(!(checkNomePrivato() && checkCognomePrivato() && checkCodiceFiscalePrivato() && checkNumeroTesseraSanitariaPrivato() && checkCittaPrivato() && checkCapPrivato() && checkViaPrivato() && checkCivicoPrivato() && checkTelefonoPrivato() && checkEmailPrivato() && checkPasswordPrivato() && checkDataNascitaPrivato())){
         event.preventDefault();
