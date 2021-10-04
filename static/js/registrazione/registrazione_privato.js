6921f8b49b2e670b45dd88894de57de5ebe878f7
DarkFighterLuke <luca-consoli@live.it>
2021-10-04 16:09:50 +0200

    Rinominate funzioni in registrazione_privato.js

--- static/js/registrazione/registrazione_privato.js
+++ static/js/registrazione/registrazione_privato.js
@@ -1,35 +1,50 @@
-var nomePrivato=document.getElementById("nome-privato");
-var cognomePrivato=document.getElementById("cognome-privato");
-var codiceFiscalePrivato=document.getElementById("codice-fiscale-privato");
-var numeroTesseraSanitariaPrivato=document.getElementById("numero-tessera-sanitaria-privato");
-var cittaPrivato=document.getElementById("citta-privato");
-var capPrivato=document.getElementById("cap-privato");
-var viaPrivato=document.getElementById("via-privato");
-var civicoPrivato=document.getElementById("civico-privato");
-var telefonoPrivato=document.getElementById("telefono-privato");
-var emailPrivato=document.getElementById("email-privato");
-var passwordPrivato=document.getElementById("password-privato");
-var confermaPasswordPrivato=document.getElementById("conferma-password-privato");
-var dataNascitaPrivato=document.getElementById("data-nascita-privato");
+var nomePrivato;
+var cognomePrivato;
+var codiceFiscalePrivato;
+var numeroTesseraSanitariaPrivato;
+var cittaPrivato;
+var capPrivato;
+var viaPrivato;
+var civicoPrivato;
+var telefonoPrivato;
+var emailPrivato;
+var passwordPrivato;
+var confermaPasswordPrivato;
+var dataNascitaPrivato;
 
-aggiungiSelectPrefissi("privato");
-aggiungiSelectPrefissi("medico");
+function initElementsPrivato(){
+    nomePrivato=document.getElementById("nome-privato");
+    cognomePrivato=document.getElementById("cognome-privato");
+    codiceFiscalePrivato=document.getElementById("codice-fiscale-privato");
+    numeroTesseraSanitariaPrivato=document.getElementById("numero-tessera-sanitaria-privato");
+    cittaPrivato=document.getElementById("citta-privato");
+    capPrivato=document.getElementById("cap-privato");
+    viaPrivato=document.getElementById("via-privato");
+    civicoPrivato=document.getElementById("civico-privato");
+    telefonoPrivato=document.getElementById("telefono-privato");
+    emailPrivato=document.getElementById("email-privato");
+    passwordPrivato=document.getElementById("password-privato");
+    confermaPasswordPrivato=document.getElementById("conferma-password-privato");
+    dataNascitaPrivato=document.getElementById("data-nascita-privato");
 
-nomePrivato.addEventListener("focusout", checkNome);
-cognomePrivato.addEventListener("focusout", checkCognome);
-codiceFiscalePrivato.addEventListener("focusout", checkCodiceFiscale);
-numeroTesseraSanitariaPrivato.addEventListener("focusout", checkNumeroTesseraSanitaria);
-cittaPrivato.addEventListener("focusout", checkCitta);
-capPrivato.addEventListener("focusout", checkCap);
-viaPrivato.addEventListener("focusout", checkVia);
-civicoPrivato.addEventListener("focusout", checkCivico);
-telefonoPrivato.addEventListener("focusout", checkTelefono);
-emailPrivato.addEventListener("focusout", checkEmail);
-passwordPrivato.addEventListener("focusout", checkPassword);
-confermaPasswordPrivato.addEventListener("focusout", checkPassword);
-dataNascitaPrivato.addEventListener("change", checkDataNascita);
+    aggiungiSelectPrefissi("privato");
 
-function checkNome(){
+    nomePrivato.addEventListener("focusout", checkNomePrivato);
+    cognomePrivato.addEventListener("focusout", checkCognomePrivato);
+    codiceFiscalePrivato.addEventListener("focusout", checkCodiceFiscalePrivato);
+    numeroTesseraSanitariaPrivato.addEventListener("focusout", checkNumeroTesseraSanitariaPrivato);
+    cittaPrivato.addEventListener("focusout", checkCittaPrivato);
+    capPrivato.addEventListener("focusout", checkCapPrivato);
+    viaPrivato.addEventListener("focusout", checkViaPrivato);
+    civicoPrivato.addEventListener("focusout", checkCivicoPrivato);
+    telefonoPrivato.addEventListener("focusout", checkTelefonoPrivato);
+    emailPrivato.addEventListener("focusout", checkEmailPrivato);
+    passwordPrivato.addEventListener("focusout", checkPasswordPrivato);
+    confermaPasswordPrivato.addEventListener("focusout", checkPasswordPrivato);
+    dataNascitaPrivato.addEventListener("change", checkDataNascitaPrivato);
+}
+
+function checkNomePrivato(){
     if(nomePrivato.value.length<1 || nomePrivato.value.length>255 || nomePrivato.value.match(/\d/)){
         nomePrivato.style.backgroundColor="red";
         return false;
@@ -40,7 +55,7 @@ function checkNome(){
     }
 }
 
-function checkCognome(){
+function checkCognomePrivato(){
     if(cognomePrivato.value.length<1 || cognomePrivato.value.length>255 || cognomePrivato.value.match(/\d/)){
         cognomePrivato.style.backgroundColor="red";
         return false;
@@ -51,7 +66,7 @@ function checkCognome(){
     }
 }
 
-function checkCodiceFiscale(){
+function checkCodiceFiscalePrivato(){
     if(codiceFiscalePrivato.value.length!==16){
         codiceFiscalePrivato.style.backgroundColor="red";
         return false;
@@ -62,7 +77,7 @@ function checkCodiceFiscale(){
     }
 }
 
-function checkNumeroTesseraSanitaria(){
+function checkNumeroTesseraSanitariaPrivato(){
     if(numeroTesseraSanitariaPrivato.value.length!==20 || !numeroTesseraSanitariaPrivato.value.match(/^[0-9]+$/)){
         numeroTesseraSanitariaPrivato.style.backgroundColor="red";
         return false;
@@ -73,7 +88,7 @@ function checkNumeroTesseraSanitaria(){
     }
 }
 
-function checkCitta(){
+function checkCittaPrivato(){
     if(cittaPrivato.value.length<1 || cittaPrivato.value.length>255 || cittaPrivato.value.match(/\d/)){
         cittaPrivato.style.backgroundColor="red";
         return false;
@@ -84,7 +99,7 @@ function checkCitta(){
     }
 }
 
-function checkCap(){
+function checkCapPrivato(){
     if(capPrivato.value.length!==5 || !capPrivato.value.match(/^[0-9]+$/)){
         capPrivato.style.backgroundColor="red";
         return false;
@@ -95,7 +110,7 @@ function checkCap(){
     }
 }
 
-function checkVia(){
+function checkViaPrivato(){
     if(viaPrivato.value.length<1 || viaPrivato.value.length>255){
         nomePrivato.style.backgroundColor="red";
         return false;
@@ -106,7 +121,7 @@ function checkVia(){
     }
 }
 
-function checkCivico(){
+function checkCivicoPrivato(){
     if(civicoPrivato.value.length<1 || civicoPrivato.value.length>4 || !civicoPrivato.value.match(/^[0-9]+$/) || civicoPrivato.value==0){
         civicoPrivato.style.backgroundColor="red";
         return false;
@@ -117,7 +132,7 @@ function checkCivico(){
     }
 }
 
-function checkTelefono(){
+function checkTelefonoPrivato(){
     if(telefonoPrivato.value.length!==10 || !telefonoPrivato.value.match(/^[0-9]+$/)){
         telefonoPrivato.style.backgroundColor="red";
         return false;
@@ -128,7 +143,7 @@ function checkTelefono(){
     }
 }
 
-function checkEmail(){
+function checkEmailPrivato(){
     if(emailPrivato.value.length<1 || emailPrivato.value.length>255 || !emailPrivato.value.match("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])")){
         emailPrivato.style.backgroundColor="red";
         return false;
@@ -139,7 +154,7 @@ function checkEmail(){
     }
 }
 
-function checkPassword(){
+function checkPasswordPrivato(){
     if(passwordPrivato.value.length<1 || passwordPrivato.value.length>255 || passwordPrivato.value!==confermaPasswordPrivato.value){
         passwordPrivato.style.backgroundColor="red";
         confermaPasswordPrivato.style.backgroundColor="red";
@@ -152,7 +167,7 @@ function checkPassword(){
     }
 }
 
-function checkDataNascita(){
+function checkDataNascitaPrivato(){
     if(dataNascitaPrivato.value==="" || !underAgeValidate(dataNascitaPrivato.value)){
         dataNascitaPrivato.style.backgroundColor="red";
         return false;
@@ -181,7 +196,7 @@ function underAgeValidate(birthday){
 }
 
 function submitPrivato(){
-    if(!(checkNome() && checkCognome() && checkCodiceFiscale() && checkNumeroTesseraSanitaria() && checkCitta() && checkCap() && checkVia() && checkCivico() && checkTelefono() && checkEmail() && checkPassword() && checkDataNascita())){
+    if(!(checkNomePrivato() && checkCognomePrivato() && checkCodiceFiscalePrivato() && checkNumeroTesseraSanitariaPrivato() && checkCittaPrivato() && checkCapPrivato() && checkViaPrivato() && checkCivicoPrivato() && checkTelefonoPrivato() && checkEmailPrivato() && checkPasswordPrivato() && checkDataNascitaPrivato())){
         event.preventDefault();
         return false;
     }
