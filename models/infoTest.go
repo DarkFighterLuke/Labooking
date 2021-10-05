096f8a70391eb5e5f4b53be77442faa6c37bf80f
DarkFighterLuke <luca-consoli@live.it>
2021-10-05 11:39:28 +0200

    Aggiunti metodi interfaccia ReadWriterDB in infoTest.go

--- models/infoTest.go
+++ models/infoTest.go
@@ -13,3 +13,31 @@ type InfoTest struct {
 	Costo         float32      `orm:"digits(4,2)"`
 	Tempi         int          `orm:""` // Tempo espresso in secondi
 }
+
+func (it *InfoTest) Aggiungi() error {
+	o := orm.NewOrm()
+	_, err := o.Insert(it)
+	return err
+}
+
+func (it *InfoTest) Seleziona(cols ...string) error {
+	o := orm.NewOrm()
+	err := o.Read(it, cols...)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (it *InfoTest) Modifica() error {
+	o := orm.NewOrm()
+	_, err := o.Update(it)
+	return err
+}
+
+func (it *InfoTest) Elimina(cols ...string) error {
+	o := orm.NewOrm()
+	err := it.Seleziona(cols...)
+	_, err = o.Delete(it)
+	return err
+}
