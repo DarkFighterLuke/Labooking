1eeb8ecd8d1e1fd16189d01af3f27265f7963f75
FrancescoBratta <fra.bra00@gmail.com>
2021-10-05 09:39:16 +0200

    Test privato

--- tests/privato_test.go
+++ tests/privato_test.go
@@ -2,9 +2,62 @@ package test
 
 import (
 	"Labooking/models"
+	"fmt"
+	"github.com/beego/beego/v2/client/orm"
+	"github.com/beego/beego/v2/server/web"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
 	"testing"
 	"time"
 )
+
+func init() {
+	//static files
+	web.SetStaticPath("/css", "static/css")
+	web.SetStaticPath("/fonts", "static/fonts")
+	web.SetStaticPath("/img", "static/img")
+	web.SetStaticPath("/js", "static/js")
+
+	//database config
+	driver, err := web.AppConfig.String("mysqldriver")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = orm.RegisterDriver(driver, orm.DRMySQL)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	mysqluser, err := web.AppConfig.String("mysqluser")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	mysqlpsw, err := web.AppConfig.String("mysqlpsw")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	mysqlurl, err := web.AppConfig.String("mysqlurl")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	mysqlport, err := web.AppConfig.String("mysqlport")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	mysqldbname, err := web.AppConfig.String("mysqldbname")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	connectionString := mysqluser + ":" + mysqlpsw + "@tcp(" +
+		mysqlurl + ":" + mysqlport + ")/" + mysqldbname +
+		"?charset=utf8"
+	err = orm.RegisterDataBase("default", driver, connectionString)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+}
 
 func TestAggiungi(t *testing.T) {
 	p := new(models.Privato)
@@ -29,5 +82,48 @@ func TestAggiungi(t *testing.T) {
 
 	if err != nil {
 		t.Errorf("err is %v; want nil", err)
+	}
+}
+
+func TestSeleziona(t *testing.T) {
+	p := new(models.Privato)
+
+	p.Nome = "Tizio"
+	p.Cognome = "Caio"
+
+	err := p.Seleziona("nome", "cognome")
+	if err != nil {
+		t.Errorf("err is %v, want nil", err)
+	}
+	fmt.Printf("%#v", p)
+	fmt.Printf("%#v", p.Medico)
+}
+
+func TestModifica(t *testing.T) {
+	p := new(models.Privato)
+
+	p.Nome = "Tizio"
+
+	err := p.Seleziona("nome")
+	if err != nil {
+		t.Errorf("err is %v, want nil", err)
+	}
+	p.Medico = new(models.Medico)
+	p.Medico.IdMedico = 1
+
+	err = p.Modifica()
+	if err != nil {
+		t.Errorf("err is %v, want nil", err)
+	}
+}
+
+func TestElimina(t *testing.T) {
+	p := new(models.Privato)
+
+	p.Nome = "Tizio"
+
+	err := p.Elimina("nome")
+	if err != nil {
+		t.Errorf("err is %v, want nil", err)
 	}
 }
