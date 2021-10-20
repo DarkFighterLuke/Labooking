0c87a9e03d99a110d85559c945b58d71af95779a
FrancescoBratta <fra.bra00@gmail.com>
2021-10-20 18:48:41 +0200

    Sostituito UserType con Ruolo

--- controllers/utils/Renderer.go
+++ controllers/utils/Renderer.go
@@ -3,7 +3,7 @@ package utils
 import "github.com/beego/beego/v2/server/web"
 
 func RenderLayout(c *web.Controller) {
-	c.Data["UserType"] = c.Ctx.Input.Session("ruolo")
+	c.Data["Ruolo"] = c.Ctx.Input.Session("ruolo")
 	c.Data["NomeUtente"] = "Nome Cognome" // TODO: Implementare logica per mostrare il nome utente
 	c.Data["NumNotifiche"] = "9+"         // TODO: Implementare logica per il contatore notifiche
 	c.Layout = "dashboard/page_content_wrapper.tpl"
