cb6734f2cc722c7be9a288c7faf0500e72de4480
DarkFighterLuke <luca-consoli@live.it>
2021-10-21 14:23:10 +0200

    Fixato parsing orari in LaboratorioController.go
    Signed-off-by: DarkFighterLuke <luca-consoli@live.it>

--- controllers/LaboratorioController.go
+++ controllers/LaboratorioController.go
@@ -3,8 +3,11 @@ package controllers
 import (
 	"Labooking/controllers/utils"
 	"Labooking/models"
+	"fmt"
 	"github.com/beego/beego/v2/server/web"
 	"strconv"
+	"strings"
+	"time"
 )
 
 type LaboratorioController struct {
@@ -69,15 +72,11 @@ func (lc *LaboratorioController) Get() {
 	var itCustom []infoTestCustom
 
 	for _, v := range its {
-		oreStr := strconv.Itoa(int(v.Tempi/3600)) + "h"
-		minutiStr := ""
-		if int(v.Tempi%3600) != 0 {
-			minutiStr = strconv.Itoa(int(v.Tempi%3600)) + "m"
-		}
+		orarioCompletoStr := parseDurationStr(v.Tempi)
 		itCustom = append(itCustom, infoTestCustom{
 			TipologiaTest: v.TipologiaTest,
 			Costo:         v.Costo,
-			TempiStr:      oreStr + " " + minutiStr,
+			TempiStr:      orarioCompletoStr,
 		})
 	}
 
@@ -86,4 +85,26 @@ func (lc *LaboratorioController) Get() {
 	lc.Data["Orari"] = orariStr
 	lc.Data["InfoTest"] = itCustom
 	lc.TplName = "dashboard/laboratorio/laboratorio.tpl"
+}
+
+func parseDurationStr(seconds int64) string {
+	tempiStr := fmt.Sprint(time.Unix(seconds, 0).Sub(time.Unix(0, 0)))
+	hIndex := strings.Index(tempiStr, "h")
+	mIndex := strings.Index(tempiStr, "m")
+
+	orarioStr := make(map[string]string)
+	if hIndex != -1 {
+		orarioStr["ore"] = tempiStr[0 : hIndex+1]
+	}
+	if mIndex != -1 {
+		orarioStr["minuti"] = tempiStr[hIndex+1 : mIndex+1]
+	}
+	orarioStr["secondi"] = tempiStr[mIndex+1:]
+	var orarioCompletoStr string
+	for _, v := range orarioStr {
+		if v[0] != '0' {
+			orarioCompletoStr += v + " "
+		}
+	}
+	return orarioCompletoStr
 }
