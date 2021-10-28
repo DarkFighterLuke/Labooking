eb1d44ee12bf8963c725ac15c2e4906799c2ce19
DarkFighterLuke <luca-consoli@live.it>
2021-10-28 12:31:39 +0200

    Implementata visualizzazione del nome dell'utente loggato nel layout

--- controllers/utils/Renderer.go
+++ controllers/utils/Renderer.go
@@ -9,7 +9,8 @@ import (
 
 func RenderLayout(c *web.Controller) {
 	c.Data["Ruolo"] = c.Ctx.Input.Session("ruolo")
-	c.Data["NomeUtente"] = "Nome Cognome" // TODO: Implementare logica per mostrare il nome utente
+	nomeUtente, err := GetNomeUtente(c)
+	c.Data["NomeUtente"] = nomeUtente
 	notifiche, err := GetNotificheByEmailPrivato(c)
 	if err == nil {
 		if len(notifiche) > 9 {
@@ -17,6 +18,7 @@ func RenderLayout(c *web.Controller) {
 		} else {
 			c.Data["NumNotifiche"] = strconv.Itoa(len(notifiche))
 		}
+		c.Data["Notifiche"] = notifiche
 	}
 
 	c.Layout = "dashboard/page_content_wrapper.tpl"
@@ -27,19 +29,16 @@ func RenderLayout(c *web.Controller) {
 
 func GetNotificheByEmailPrivato(c *web.Controller) ([]*models.TestDiagnostico, error) {
 	email := c.GetSession("email")
-	if email == "" {
-		return nil, fmt.Errorf("notifiche: %s", "email assente")
-	}
 	ruolo := c.GetSession("ruolo")
 	if ruolo != "privato" {
-		return nil, fmt.Errorf("notifiche: %s", "ruolo diverso da privato")
+		return nil, fmt.Errorf("getnotifiche: %s", "ruolo diverso da privato")
 	}
 
 	p := new(models.Privato)
 	p.Email = email.(string)
 	err := p.Seleziona("email")
 	if err != nil {
-		return nil, fmt.Errorf("notifiche: %s", err.Error())
+		return nil, fmt.Errorf("getnotifiche: %s", err.Error())
 	}
 
 	td := new(models.TestDiagnostico)
@@ -51,4 +50,45 @@ func GetNotificheByEmailPrivato(c *web.Controller) ([]*models.TestDiagnostico, e
 	}
 
 	return testDiagnostici, err
+}
+
+func GetNomeUtente(c *web.Controller) (string, error) {
+	email := c.GetSession("email")
+	ruolo := c.GetSession("ruolo")
+
+	if ruolo == "privato" {
+		p := new(models.Privato)
+		p.Email = email.(string)
+		err := p.Seleziona("email")
+		if err != nil {
+			return "", fmt.Errorf("getnomeutente: %s", err.Error())
+		}
+		return p.Nome + " " + p.Cognome, err
+	} else if ruolo == "medico" {
+		m := new(models.Medico)
+		m.Email = email.(string)
+		err := m.Seleziona("email")
+		if err != nil {
+			return "", fmt.Errorf("getnomeutente: %s", err.Error())
+		}
+		return m.Nome + " " + m.Cognome, err
+	} else if ruolo == "organizzazione" {
+		o := new(models.Organizzazione)
+		o.Email = email.(string)
+		err := o.Seleziona("email")
+		if err != nil {
+			return "", fmt.Errorf("getnomeutente: %s", err.Error())
+		}
+		return o.RagioneSociale, err
+	} else if ruolo == "laboratorio" {
+		l := new(models.Laboratorio)
+		l.Email = email.(string)
+		err := l.Seleziona("email")
+		if err != nil {
+			return "", fmt.Errorf("getnomeutente: %s", err.Error())
+		}
+		return l.Nome, err
+	} else {
+		return "", fmt.Errorf("getnomeutente: %s", "ruolo sconosciuto")
+	}
 }
